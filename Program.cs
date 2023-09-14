22e5c2c42ec007d1f20577c97c048e27f8ae39d8
aasli <aasli@ASLINB>
2023-09-14 13:35:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MainClass
+{
+
+    static int FirstFactorial(int num)
+    {
+        // code goes here  
+        if (num <= 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return num * FirstFactorial(num - 1);
+        }
+    }
+
+    static void Main()
+    {
+        // keep this function call here
+        int num = 4;
+        int result = FirstFactorial(num);
+        Console.WriteLine("The factorial " + num + " is " + result);
+    }
+}
